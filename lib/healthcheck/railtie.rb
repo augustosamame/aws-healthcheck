7c58a7d1cd60bfb2157ce2471f90285a05331e5d
Augusto Samamé <augustosamame@gmail.com>
2017-05-09 17:15:02 -0500

    fixed middleware for when use in railtie

--- lib/healthcheck/railtie.rb
+++ lib/healthcheck/railtie.rb
@@ -2,6 +2,6 @@ require 'rails'
 
 module Healthcheck
   class Railtie < ::Rails::Railtie
-    config.app.middleware.insert_before ::Rails::Rack::Logger, Healthcheck::Middleware
+    config.app_middleware.insert_before ::Rails::Rack::Logger, Healthcheck::Middleware
   end
 end
