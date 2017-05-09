2ca49265f3720edbe9c151319fc622afe96291cb
Augusto Samamé <augustosamame@gmail.com>
2017-05-09 13:32:53 -0500

    fix silencer for Timber logger

--- lib/healthcheck/railtie.rb
+++ lib/healthcheck/railtie.rb
@@ -2,6 +2,6 @@ require 'rails'
 
 module Healthcheck
   class Railtie < ::Rails::Railtie
-    config.app_middleware.use Healthcheck::Middleware
+    config.app.middleware.insert_before ::Rails::Rack::Logger, Healthcheck::Middleware
   end
 end
